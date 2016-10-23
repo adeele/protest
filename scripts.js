6f8b4d233231eb976c0a74482a95e5a6a422ec38
adele <adrianna.malkiewicz@gmail.com>
2016-10-23 14:57:41 +0200

    Main page

--- scripts.js
+++ scripts.js
@@ -1,15 +1,4 @@
-Vue.component('home', {
-  template: '<img src="protest.jpg">'
-})
-
-Vue.component('list', {
-  template: '<h1>LIST</h1>'
-})
-
-Vue.component('event', {
-  template: '<h1>EVENT</h1>'
-})
-
-new Vue({
-  el: '#app'
-})
+$('h2').click(function() {
+  $('html, body').animate({ scrollTop: $('#list').offset().top }, 'slow');
+  return false;
+});
